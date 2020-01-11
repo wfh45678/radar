15bc43aa629f9447595d4fdd786492ceeaba0dfd
feihu.wang <wfh45678@163.com>
2020-01-11 12:02:05 +0800

    resources: sql and machine learning model
    Signed-off-by: feihu.wang <wfh45678@163.com>

--- sql/radar-1.0.3.sql
+++ sql/radar-1.0.3.sql
@@ -20,35 +20,29 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `engine_model_conf`;
 CREATE TABLE `engine_model_conf` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `model_id` int(11) DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `model_id` bigint(20) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `path` varchar(255) DEFAULT NULL,
   `tag` varchar(255) DEFAULT NULL,
   `operation` varchar(255) DEFAULT NULL,
   `update_date` datetime DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
+  `comment` varchar(128) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of engine_model_conf
--- ----------------------------
-INSERT INTO `engine_model_conf` VALUES ('1', '103', '交易ai模型', 'd:/radar01', 'serve', 'output_y/BiasAdd', '2019-12-24 17:38:38', 'TENSOR_DNN');
 
 -- ----------------------------
 -- Table structure for engine_model_conf_param
 -- ----------------------------
 DROP TABLE IF EXISTS `engine_model_conf_param`;
 CREATE TABLE `engine_model_conf_param` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mold_id` int(11) DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `mold_id` bigint(20) DEFAULT NULL,
   `feed` varchar(255) DEFAULT NULL,
   `expressions` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of engine_model_conf_param
--- ----------------------------
-INSERT INTO `engine_model_conf_param` VALUES ('1', '1', 'input_x', 'abstractions.tran_uid_ip_1_day_qty,abstractions.tran_did_ip_1_day_qty,abstractions.tran_ip_1_day_qty,abstractions.tran_ip_1_hour_qty,abstractions.tran_ip_1_day_amt,abstractions.tran_did_1_day_qty');
